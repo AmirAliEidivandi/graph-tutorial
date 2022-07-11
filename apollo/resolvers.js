f594b8cd1af96c11dd66512ccb73a85543d8dbb6
AmirAliEidivandi <amirah648887@gmail.com>
2022-07-11 09:59:51 +0430

    create http methods

--- apollo/resolvers.js
+++ apollo/resolvers.js
@@ -8,6 +8,9 @@ const resolvers = {
         getAllPosts: async () => {
             return await Post.find();
         },
+        getPost: async (_parent, { id }, _context, _info) => {
+            return await Post.findById(id);
+        },
     },
     Mutation: {
         createPost: async (parent, args, context, info) => {
@@ -15,6 +18,26 @@ const resolvers = {
             const post = new Post({ title, description });
 
             await post.save();
+            return post;
+        },
+        deletePost: async (parent, { id }, context, info) => {
+            await Post.findByIdAndDelete(id);
+            return "user has been deleted....";
+        },
+        updatePost: async (parent, args, context, info) => {
+            const { id } = args;
+            const { title, description } = args.post;
+            const updates = {};
+
+            if (title !== undefined) {
+                updates.title = title;
+            }
+
+            if (description !== undefined) {
+                updates.description = description;
+            }
+
+            const post = await Post.findByIdAndUpdate(id, updates, { new: true });
             return post;
         },
     },
