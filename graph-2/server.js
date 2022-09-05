54a945a64f54584cb1705e338783619fd8114ab5
AmirAliEidivandi <amirah648887@gmail.com>
2022-09-05 17:33:44 +0430

    change project to apollo-server

--- graph-2/server.js
+++ graph-2/server.js
@@ -1,25 +1,29 @@
 const path = require("path");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
-const app = express();
+const { ApolloServer } = require("apollo-server-express");
 
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.gql"));
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const schema = makeExecutableSchema({
-    typeDefs: typesArray,
-    resolvers: resolversArray,
-});
+const startApolloServer = async () => {
+    const app = express();
 
-app.use(
-    "/graphql",
-    graphqlHTTP({
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray,
+    });
+
+    const server = new ApolloServer({
         schema,
-        graphiql: true,
-    })
-);
+    });
 
-const port = 8000;
-app.listen(port, () => console.log("listening on port 8000"));
+    await server.start();
+    server.applyMiddleware({ app, path: "/graphql" });
+
+    const port = 8000;
+    app.listen(port, () => console.log("listening on port 8000"));
+};
+
+startApolloServer();
