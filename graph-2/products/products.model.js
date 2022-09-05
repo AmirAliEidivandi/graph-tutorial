d7ef2f14f5577472a898de89bf0988ef3a90891d
AmirAliEidivandi <amirah648887@gmail.com>
2022-09-05 16:51:30 +0430

    complete project

--- graph-2/products/products.model.js
+++ graph-2/products/products.model.js
@@ -3,21 +3,51 @@ const products = [
         id: "h1",
         description: "hello-1",
         price: 42.5,
+        reviews: [],
     },
     {
         id: "h2",
         description: "hello-2",
         price: 70.4,
+        reviews: [],
     },
 ];
 
-function getAllProducts() {
-    return products;
-}
+const getAllProducts = () => products;
 
 const getProductsByPrice = (min, max) => products.filter((product) => product.price >= min && product.price <= max);
+
+const getProductById = (id) => products.find((product) => product.id === id);
+
+const addNewProduct = (id, description, price) => {
+    const newProduct = {
+        id,
+        description,
+        price,
+        reviews: [],
+    };
+    products.push(newProduct);
+    return newProduct;
+};
+
+const addNewProductReview = (id, rating, comment) => {
+    const matchedProduct = getProductById(id);
+
+    if (matchedProduct) {
+        const newProductReview = {
+            rating,
+            comment,
+        };
+
+        matchedProduct.reviews.push(newProductReview);
+        return newProductReview;
+    }
+};
 
 module.exports = {
     getAllProducts,
     getProductsByPrice,
+    getProductById,
+    addNewProduct,
+    addNewProductReview,
 };
--- graph-2/products/products.resolvers.js
+++ graph-2/products/products.resolvers.js
@@ -1,8 +1,13 @@
-const { getAllProducts, getProductsByPrice } = require("./products.model");
+const { getAllProducts, getProductsByPrice, getProductById, addNewProduct, addNewProductReview } = require("./products.model");
 
 module.exports = {
     Query: {
         products: () => getAllProducts(),
         productsByPrice: (_, args) => getProductsByPrice(args.min, args.max),
+        product: (_, args) => getProductById(args.id),
+    },
+    Mutation: {
+        addNewProduct: (_, args) => addNewProduct(args.id, args.description, args.price),
+        addNewProductReview: (_, args) => addNewProductReview(args.id, args.rating, args.comment),
     },
 };
